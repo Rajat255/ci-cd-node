8d0e7c039b3d2a1d9017bc413f06731dea048aeb
RajatK <78610455+Rajat255@users.noreply.github.com>
2023-10-25 13:44:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(express.json()); app.use(cors());
 app.use((req, res, next) => { res.locals.path = req.path; next(); });
 app.use("/api/cruds", crudRoutes); 
 //app.use("/api/auth", authRoute);
-app.listen(PORT, '0.0.0.0',() => console.log("Listening on port "${PORT}"..."));
+app.listen(PORT, '0.0.0.0',() => console.log("Listening on port ${PORT}..."));
 
