5722152772047c502c446d913ae6846556458c0c
RajatK <78610455+Rajat255@users.noreply.github.com>
2023-10-25 13:42:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const connection = require("./db");
 const crudRoutes = require("./routes/crudRoutes");
 const app = express(); const PORT = 8080;
 
-connection connection();
+connection();
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); app.use(cors()); 
 app.use((req, res, next) => { res.locals.path = req.path; next(); });
 app.use("/api/cruds", crudRoutes); 
 //app.use("/api/auth", authRoute);
-app.listen(PORT, '0.0.0.0',() => console.log(Listening on port ${PORT}...));
+app.listen(PORT, '0.0.0.0',() => console.log("Listening on port "${PORT}"..."));
 
